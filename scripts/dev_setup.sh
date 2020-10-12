9ce483b72bf3b75d4e1085e64d878e1915356e66
JonSteinn <jonsteinn@gmail.com>
2020-10-12 21:36:50 +0000

    fix dep

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 -m pip install -r requirements-dev.txt
 python3 setup.py egg_info
-python3 -m pip install -r iceart.egg-info\requires.txt
+python3 -m pip install -r iceart.egg-info/requires.txt
 pre-commit install
